656d090c4336445554364dbe4328eb9dce114e9d
James M. Berick <james@James-Bericks-MacBook-Pro-3.local>
2015-11-06 10:45:53 -0500

    (logins added)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106142051) do
+ActiveRecord::Schema.define(version: 20151106154056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,11 @@ ActiveRecord::Schema.define(version: 20151106142051) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.text     "password"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "password_digest"
+    t.string   "salt"
+    t.string   "encrypted_password"
   end
 
 end
